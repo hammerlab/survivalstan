829d04920e4828d883c710386635726e8606f4bc
Jacqueline Buros Novik <jackinovik@gmail.com>
2016-06-29 16:47:04 -0400

    add varying-coef weibull model

--- stanmodels/stan.py
+++ stanmodels/stan.py
@@ -1,7 +1,14 @@
 import pkg_resources, os
 
 resource_package = __name__  ## Could be any module/package name.
-pem_survival_path = os.path.join('stan', 'pem_survival_model.stan')
-weibull_survival_path = os.path.join('stan', 'weibull_survival_model.stan')
-pem_survival_model = pkg_resources.resource_string(resource_package, pem_survival_path).decode("utf-8") 
-weibull_survival_model = pkg_resources.resource_string(resource_package, weibull_survival_path).decode("utf-8") 
+
+_pem_survival_path = os.path.join('stan', 'pem_survival_model.stan')
+_weibull_survival_path = os.path.join('stan', 'weibull_survival_model.stan')
+_weibull_survival_varcoef_path = os.path.join('stan', 'weibull_survival_model_varying_coefs.stan')
+
+pem_survival_model = pkg_resources.resource_string(
+	resource_package, _pem_survival_path).decode("utf-8") 
+weibull_survival_model = pkg_resources.resource_string(
+	resource_package, _weibull_survival_path).decode("utf-8") 
+weibull_survival_model_varying_coefs = pkg_resources.resource_string(
+	resource_package, _weibull_survival_varcoef_path).decode("utf-8") 
