0dbcef0c1b507bfc6c46035595ce378c63ad6766
Jacki Buros <jackinovik@gmail.com>
2016-06-29 18:11:11 +0000

    first attempt to include functions & stan files

--- stanmodels/__init__.py
+++ stanmodels/__init__.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from .survival_model import *
 
 from ._version import get_versions
 __version__ = get_versions()['version']
