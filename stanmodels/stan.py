b8d0263688ddac3665ebcdb8ddf7f87755968309
Jacqueline Buros Novik <jackinovik@gmail.com>
2016-06-29 16:10:49 -0400

    decode bytes to UTF-8

--- stanmodels/stan.py
+++ stanmodels/stan.py
@@ -3,5 +3,5 @@ import pkg_resources, os
 resource_package = __name__  ## Could be any module/package name.
 pem_survival_path = os.path.join('stan', 'pem_survival_model.stan')
 weibull_survival_path = os.path.join('stan', 'weibull_survival_model.stan')
-pem_survival_model = pkg_resources.resource_string(resource_package, pem_survival_path)
-weibull_survival_model = pkg_resources.resource_string(resource_package, weibull_survival_path)
+pem_survival_model = pkg_resources.resource_string(resource_package, pem_survival_path).decode("utf-8") 
+weibull_survival_model = pkg_resources.resource_string(resource_package, weibull_survival_path).decode("utf-8") 
