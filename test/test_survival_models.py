525083841f0fe14072aada93187576966639707c
Jacqueline Buros Novik <jackinovik@gmail.com>
2016-06-29 22:04:54 -0400

    add test code for varyingcoef pem model

--- test/test_survival_models.py
+++ test/test_survival_models.py
@@ -52,3 +52,25 @@ def test_pem_model():
 	ok_('loo' in testfit)
 	return(testfit)
 
+def test_pem_varcoef_model():
+	''' Test Weibull survival model on test dataset
+	'''
+	d = load_test_dataset()
+	dlong = stanmodels.prep_data_long_surv(d, time_col = 'futime', event_col = 'death')
+	testfit = stanmodels.fit_stan_survival_model(
+		model_cohort = 'pem varcoef model',
+		model_code = stanmodels.stan.pem_survival_model_varying_coefs,
+		df = dlong,
+		sample_col = 'index',
+		timepoint_end_col = 'end_time',
+		event_col = 'end_failure',
+		group_col = 'sex',
+		formula = 'age',
+		iter = 3000,
+		chains = 4,
+		)
+	ok_('fit' in testfit)
+	ok_('coefs' in testfit)
+	ok_('loo' in testfit)
+	return(testfit)
+
