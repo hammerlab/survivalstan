afdc7c6fdd12aa27a136dba5d9f10be3ab2e8c55
Jacki Buros <jackinovik@gmail.com>
2016-07-13 15:36:26 +0000

    fix __init__.py

--- survivalstan/__init__.py
+++ survivalstan/__init__.py
@@ -12,9 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import .utils as utils
-import .models as models
-
+import utils
+import models
 
 from ._version import get_versions
 __version__ = get_versions()['version']
