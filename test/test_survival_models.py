dbe828a1d0c46a75fa2567fc7a1452b6cfb975e4
Jacqueline Buros Novik <jackinovik@gmail.com>
2016-07-16 10:36:53 -0400

    modify package name in tests

--- test/test_survival_models.py
+++ test/test_survival_models.py
@@ -1,6 +1,6 @@
 
 import statsmodels
-import stanmodels
+import survivalstan
 from nose.tools import ok_
 
 def load_test_dataset():
@@ -15,16 +15,16 @@ def test_weibull_model():
 	''' Test Weibull survival model on test dataset
 	'''
 	d = load_test_dataset()
-	testfit = stanmodels.fit_stan_survival_model(
+	testfit = survivalstan.fit_stan_survival_model(
 		model_cohort = 'test model',
-		model_code = stanmodels.stan.weibull_survival_model,
+		model_code = survivalstan.models.weibull_survival_model,
 		df = d,
 		time_col = 'futime',
 		event_col = 'death',
 		formula = 'age + sex',
 		iter = 3000,
 		chains = 4,
-		make_inits = stanmodels.make_weibull_survival_model_inits
+		make_inits = survivalstan.make_weibull_survival_model_inits
 		)
 	ok_('fit' in testfit)
 	ok_('coefs' in testfit)
@@ -35,10 +35,10 @@ def test_pem_model():
 	''' Test Weibull survival model on test dataset
 	'''
 	d = load_test_dataset()
-	dlong = stanmodels.prep_data_long_surv(d, time_col = 'futime', event_col = 'death')
-	testfit = stanmodels.fit_stan_survival_model(
+	dlong = survivalstan.prep_data_long_surv(d, time_col = 'futime', event_col = 'death')
+	testfit = survivalstan.fit_stan_survival_model(
 		model_cohort = 'test model',
-		model_code = stanmodels.stan.pem_survival_model,
+		model_code = survivalstan.models.pem_survival_model,
 		df = dlong,
 		sample_col = 'index',
 		timepoint_end_col = 'end_time',
@@ -56,10 +56,10 @@ def test_pem_varcoef_model():
 	''' Test varying-coef PEM survival model on test dataset
 	'''
 	d = load_test_dataset()
-	dlong = stanmodels.prep_data_long_surv(d, time_col = 'futime', event_col = 'death')
-	testfit = stanmodels.fit_stan_survival_model(
+	dlong = survivalstan.prep_data_long_surv(d, time_col = 'futime', event_col = 'death')
+	testfit = survivalstan.fit_stan_survival_model(
 		model_cohort = 'pem varcoef model',
-		model_code = stanmodels.stan.pem_survival_model_varying_coefs,
+		model_code = survivalstan.models.pem_survival_model_varying_coefs,
 		df = dlong,
 		sample_col = 'index',
 		timepoint_end_col = 'end_time',
