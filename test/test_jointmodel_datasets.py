f02957eb64f1648a3492e8785c13497aa6744e30
Jacki Novik <jackinovik@gmail.com>
2017-01-09 23:30:25 +0000

    lint fixes to test code

--- test/test_jointmodel_datasets.py
+++ test/test_jointmodel_datasets.py
@@ -3,13 +3,15 @@ import survivalstan
 import random
 random.seed(9001)
 
+import pandas as pd 
+
 def sim_test_dataset_long(n=200):
     data = survivalstan.sim.sim_data_jointmodel(N=n)
-    ldf = survivalstan.prep_data_surv_long(data['events'],
+    ldf = survivalstan.prep_data_long_surv(data['events'],
                                            event_col='event_value',
                                            event_name='event_name',
                                            time_col='time',
                                            sample_col='subject_id')
-    ldf = pd.merge(ldf, x_df, on='subject_id', how='outer')
+    ldf = pd.merge(ldf, data['covars'], on='subject_id', how='outer')
     return(ldf)
 
