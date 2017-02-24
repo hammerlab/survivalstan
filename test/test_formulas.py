d715db73f2f857594d1beb8d7898858eb1de4e04
Jacki Novik <jackinovik@gmail.com>
2017-02-24 20:04:48 +0000

    work in progress on surv-related tests

--- test/test_formulas.py
+++ test/test_formulas.py
@@ -35,18 +35,81 @@ def test_as_id_formula():
     df = get_test_data()
     y, X = patsy.dmatrices(formula_like=test_formula, data=df)
     res = pd.DataFrame(y)
-    # should have 3 columns
+    # should have 3 columns & same number rows as df
     eq_(res.shape[1], 3)
-    # should be numeric type
-    ok_(res[1].dtype == np.float64)
-    ok_(res[2].dtype == np.float64)
-    # should have min value of 1
-    eq_(np.min(res[1]), 1)
-    eq_(np.min(res[2]), 1)
-    # should have max value equal to number of unique values
-    eq_(np.max(res[1]), len(df['time'].unique()))
-    eq_(np.max(res[2]), len(df['subject_id'].unique()))
+    eq_(res.shape[0], df.shape[0])
+    # check valid ids
+    ok_(check_valid_id(res[1], ref=df['time']))
+    ok_(check_valid_id(res[2], ref=df['subject_id']))
+
+def is_sequential(x):
+    it = (int(el, 16) for el in x)
+    first = next(it)
+    return all(a == b for a, b in enumerate(it, first + 1))
+
+def check_valid_id(x, ref=None):
+    ''' helper function to validate whether x is an ID
+    '''
+    ok_(is_sequential(x))
+    eq_(np.min(x), 1)
     # TODO test one-to-one & onto relationship
+    if ref:
+        eq_(np.max(x), len(x.unique()))
+
+def test_surv_df():
+    ''' test that surv stateful transform accepts time & event values
+    '''
+    df = get_test_data()
+    res = surv(time=df['time'], event_status=df['event_value'])
+    eq_(res.shape[1], 2)
+    eq_(res.shape[0], len(df.index))
+    ok_(array_equal(res.columns, ['event_status', 'event_time']))
+    eq_(np.sum(res['event_status']), np.sum(df['event_value']))
+
+def test_surv_df_subject():
+    ''' test that surv stateful transform includes subject id when given
+    '''
+    df = get_test_data()
+    res = surv(time=df['time'], event_status=df['event_value'],
+               subject=df['subject_id'])
+    eq_(res.shape[1], 3)
+    eq_(res.shape[0], len(df.index))
+    ok_(array_equal(res.columns, ['event_status', 'subject_id',
+                                  'timepoint_id']))
+    ok_(check_valid_id(res['subject_id'], ref=df['subject_id']))
+    ok_(check_valid_id(res['timepoint_id'], ref=df['time']))
+    eq_(np.sum(res['event_status']), np.sum(df['event_value']))
+
+def test_surv_df_formula():
+    df = get_test_data()
+    y, X = patsy.dmatrices('surv(time=time, event_status=event_value) ~ X1',
+                           data=df)
+    res = pd.DataFrame(y)
+    eq_(res.shape[1], 2)
+    eq_(res.shape[0], len(df.index))
+    eq_(np.sum(res[0]), np.sum(df['event_value']))
+    eq_(np.sum(res[1]), np.sum(df['time']))
+
+def test_surv_df_subject_formula():
+    df = get_test_data()
+    y, X = patsy.dmatrices('surv(time=time, event_status=event_value,
+                                  subject=subject_id) ~ X1', data=df)
+    res = pd.DataFrame(y)
+    # test quality of res
+    eq_(res.shape[1], 3)
+    eq_(res.shape[0], len(df.index))
+    ok_(check_valid_id(res[1], ref=df['subject_id']))
+    ok_(check_valid_id(res[2], ref=df['time']))
+    eq_(np.sum(res[0]), np.sum(df['event_value']))
+    # test whether class ids are retained when predicting new data
+    (y.new, X.new) = patsy.build_design_matrices([y.design_info,
+                                                   X.design_info], df.tail()) 
+    res2 = pd.DataFrame(y.new)
+    resm = pd.merge(res, res2, on=[1,2], how='inner')
+
+
+
+
 
 
 
